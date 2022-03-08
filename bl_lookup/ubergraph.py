e191a253c34d36fd6bd727ae0a02788891871025
Chris Bizon <bizon@email.unc.edu>
2022-03-08 16:30:38 -0500

    changed ubergraph location

--- bl_lookup/ubergraph.py
+++ bl_lookup/ubergraph.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 class UberGraph:
 
     def __init__(self):
-        self.triplestore = TripleStore("https://stars-app.renci.org/uberongraph/sparql")
+        self.triplestore = TripleStore("https://ubergraph.apps.renci.org/sparql")
 
     def is_subclass(self, parent, child):
         return False
